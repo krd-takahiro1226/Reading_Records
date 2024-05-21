0bd1a993f33e9fe051003ef7f10b7d3b2da78ecc
Chosso <kurotak7255@gmail.com>
2024-05-21 21:22:41 +0900

    Repositoryアノテーションを追加

--- backend/backend/src/main/java/com/readrecords/backend/repository/ReadRecordsRepository.java
+++ backend/backend/src/main/java/com/readrecords/backend/repository/ReadRecordsRepository.java
@@ -2,7 +2,8 @@ package com.readrecords.backend.repository;
 
 import com.readrecords.backend.entity.ReadRecords;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ReadRecordsRepository extends CrudRepository<ReadRecords, Integer> {
 }
