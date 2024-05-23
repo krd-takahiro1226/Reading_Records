dbf4d266768face99311e7d4ee380d2c52a42d8b
Chosso <kurotak7255@gmail.com>
2024-05-23 22:38:37 +0900

    @Modifyingアノテーションの追加(必要だと書いてあったがあまり調べていない)

--- backend/backend/src/main/java/com/readrecords/backend/repository/ReadRecordsRepository.java
+++ backend/backend/src/main/java/com/readrecords/backend/repository/ReadRecordsRepository.java
@@ -2,6 +2,7 @@ package com.readrecords.backend.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,8 +11,10 @@ import com.readrecords.backend.entity.ReadRecords;
 
 @Repository
 public interface ReadRecordsRepository extends CrudRepository<ReadRecords, Integer> {
+  @Modifying
   @Query("select * from readrecords where book_name = :book_name")
   Optional<ReadRecords> findByBookname(String book_name);
+  @Modifying
   @Query("select * from readrecords where author = :author")
   Optional<ReadRecords> findByAuthor(String author);
   // ISBNをカラムに追加したら実装するようにする
