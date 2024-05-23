6f42475d1ba5d0593029f3f4b3b970bad21f35c3
Chosso <kurotak7255@gmail.com>
2024-05-23 22:27:15 +0900

    sqlのファイル名およびテーブル名を変更

--- sql/create_read_records.sql
+++ sql/create_read_records.sql
@@ -1,5 +1,5 @@
-CREATE TABLE reading_records (
-    book_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE read_records (
+    book_id INT NOT NULL AUTO_INCREMENT,PRIMARY KEY (book_id),
     book_name VARCHAR(30) NOT NULL,
     author VARCHAR(30) NOT NULL,
     start_date DATE,
@@ -7,6 +7,5 @@ CREATE TABLE reading_records (
     read_count INT,
     genre VARCHAR(20),
     priority INT,
-    memo VARCHAR(100),
-    PRIMARY KEY (book_name)
+    memo VARCHAR(100)
 );
