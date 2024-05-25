2ac0d374ed362d3d480f1f6945d3eeb323c8795a
Chosso <kurotak7255@gmail.com>
2024-05-25 15:55:45 +0900

    pomとRepositoryの記述を修正→mvn clean installの動作確認

--- backend/backend/src/main/java/com/readrecords/backend/repository/ReadRecordsRepository.java
+++ backend/backend/src/main/java/com/readrecords/backend/repository/ReadRecordsRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.readrecords.backend.entity.ReadRecords;
@@ -12,11 +13,11 @@ import com.readrecords.backend.entity.ReadRecords;
 @Repository
 public interface ReadRecordsRepository extends CrudRepository<ReadRecords, Integer> {
   @Modifying
-  @Query("select * from readrecords where book_name = :book_name")
-  Optional<ReadRecords> findByBookname(String book_name);
+  @Query(value="select * from readrecords" + "where book_name = :book_name",nativeQuery = true)
+  Optional<ReadRecords> findByBookname(@Param("book_name") String book_name);
   @Modifying
-  @Query("select * from readrecords where author = :author")
-  Optional<ReadRecords> findByAuthor(String author);
+  @Query(value="select * from readrecords" + "where author = :author",nativeQuery = true)
+  Optional<ReadRecords> findByAuthor(@Param ("author") String author);
   // ISBNをカラムに追加したら実装するようにする
   // @Query("select * from readrecords where ISBN = :ISBN")
   // ptional<ReadRecords>  findByISBN(String ISBN);
