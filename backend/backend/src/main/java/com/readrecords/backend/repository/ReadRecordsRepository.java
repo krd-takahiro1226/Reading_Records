21be04caef1da08c9f3a0440e5c8357ead7f168f
Chosso <kurotak7255@gmail.com>
2024-05-26 00:17:16 +0900

    新しい本を登録する際のメソッドを修正

--- backend/backend/src/main/java/com/readrecords/backend/repository/ReadRecordsRepository.java
+++ backend/backend/src/main/java/com/readrecords/backend/repository/ReadRecordsRepository.java
@@ -1,5 +1,6 @@
 package com.readrecords.backend.repository;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Modifying;
@@ -12,6 +13,15 @@ import com.readrecords.backend.entity.ReadRecords;
 
 @Repository
 public interface ReadRecordsRepository extends CrudRepository<ReadRecords, Integer> {
+  @Modifying
+  @Query(value="INSERT INTO readrecords VALUES(:book_name, :author, :start_date, :read_count, :genre, :priority, :memo)",nativeQuery = true)
+  void createReadRecords(@Param("book_name") String book_name
+                        ,@Param("author") String author
+                        ,@Param("start_date") Date start_date
+                        ,@Param("read_count") Integer read_count
+                        ,@Param("genre") String genre
+                        ,@Param("priority") Integer priority
+                        ,@Param("memo") String memo);
   @Modifying
   @Query(value="select * from readrecords" + "where book_name = :book_name",nativeQuery = true)
   Optional<ReadRecords> findByBookname(@Param("book_name") String book_name);
