e60da42bf7539322d1d915dbaff4756c66495fe2
Chosso <kurotak7255@gmail.com>
2024-06-04 22:46:23 +0900

    複数のEntityを使用する際、テーブルの見分けをつけるためのアノテーションを付与

--- backend/backend/src/main/java/com/readrecords/backend/entity/UserLogin.java
+++ backend/backend/src/main/java/com/readrecords/backend/entity/UserLogin.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 @Data
 @Entity
+@Table(name = "user_login")
 public class UserLogin {
   @Id
   private Integer user_id;
