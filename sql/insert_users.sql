137f863653f066361b4f19f867bbb109ffea9885
Chosso <kurotak7255@gmail.com>
2024-06-08 20:09:41 +0900

    テスト用のユーザパスワードをハッシュ化したものに変更

--- sql/insert_users.sql
+++ sql/insert_users.sql
@@ -1,2 +1,2 @@
 insert users (id, username, email, password, role, created_at, updated_at)
-values (1, "黒田", "hogehoge@email.com", "hogehoge", "admin", "2020-01-01", "2020-01-01");
+values (1, "黒田", "hogehoge@email.com", "$2a$08$027qeXvBhW2fu//MYlsyBe9Gz3zUZVXwoGZaaPefHHzN61BTWuOTm", "admin", "2020-01-01", "2020-01-01");
