9a353900c40d590107f851e149f55b2b95ead392
Chosso <kurotak7255@gmail.com>
2024-06-09 01:01:40 +0900

    カラム名idをuser_idに変更

--- sql/insert_users.sql
+++ sql/insert_users.sql
@@ -1,2 +1,2 @@
-insert users (id, username, email, password, role, created_at, updated_at)
-values (1, "黒田", "hogehoge@email.com", "$2a$08$027qeXvBhW2fu//MYlsyBe9Gz3zUZVXwoGZaaPefHHzN61BTWuOTm", "admin", "2020-01-01", "2020-01-01");
+insert users (user_id, username, email, password, role, created_at, updated_at)
+values (1, "test_user", "hogehoge@email.com", "$2a$08$027qeXvBhW2fu//MYlsyBe9Gz3zUZVXwoGZaaPefHHzN61BTWuOTm", "admin", "2020-01-01", "2020-01-01");
