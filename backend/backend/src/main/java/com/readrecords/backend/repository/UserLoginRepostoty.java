8c533879235460945125fcd50630a385e895a1fb
Chosso <kurotak7255@gmail.com>
2024-06-09 18:39:47 +0900

    クエリを修正

--- backend/backend/src/main/java/com/readrecords/backend/repository/UserLoginRepostoty.java
+++ backend/backend/src/main/java/com/readrecords/backend/repository/UserLoginRepostoty.java
@@ -2,17 +2,18 @@ package com.readrecords.backend.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.readrecords.backend.entity.UserLogin;
 
 @Repository
-public interface UserLoginRepostoty extends CrudRepository<UserLogin, Integer> {
-  @Modifying
-  @Query(value = "select * from users" + "where username = :username", nativeQuery = true)
-  Optional<UserLogin> findByUsername(@Param("username") String user_name);
+public interface UserLoginRepostoty extends JpaRepository<UserLogin, Long> {
+  @Query(value = "select * from users " + "where username = :username", nativeQuery = true)
+  Optional<UserLogin> findByUsername(@Param("username") String username);
+
+  @Query(value = "select username, password, role from users " + "where username = :username", nativeQuery = true)
+  String findPasswordByUsername(@Param("username") String username);
 }
