6b09a0f12d2278b19a16fb0b753ec48919f5a25f
Chosso <kurotak7255@gmail.com>
2024-06-09 19:19:20 +0900

    ログインに関するControllerを追記

--- backend/backend/src/main/java/com/readrecords/backend/controller/UserLoginController.java
+++ backend/backend/src/main/java/com/readrecords/backend/controller/UserLoginController.java
@@ -1,11 +1,14 @@
 package com.readrecords.backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.readrecords.backend.repository.UserLoginRepostoty;
+import com.readrecords.backend.security.UserLoginDetails;
 import com.readrecords.backend.service.UserLoginDetailsService;
 
 @Controller
@@ -13,13 +16,21 @@ import com.readrecords.backend.service.UserLoginDetailsService;
 public class UserLoginController {
   @Autowired
   UserLoginDetailsService userLoginDetailsService;
-  @GetMapping
-  public UserDetails loadUserByUsername(String username) {
-    return userLoginDetailsService.loadUserByUsername(username);
+  @Autowired
+  PasswordEncoder passwordEncoder;
+  @Autowired
+  UserLoginRepostoty userLoginRepostoty;
+  @RequestMapping(method = RequestMethod.GET)
+  public String showLoginWindow() {
+    return "user_login";
   }
-  @GetMapping
-  public String login() {
-    return "login";
+  @RequestMapping(method = RequestMethod.POST)
+  public String executeLogin(String username, String password, Model model) {
+    String collectPassword = userLoginRepostoty.findPasswordByUsername(username);
+    if (passwordEncoder.matches(password, collectPassword)) {
+      return "redirect:/show";
+    }
+    else
+      return "redirect:/user_login";
   }
-  
 }
