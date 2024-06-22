77ceb7ea25c5b9e00422d33579c1e50679cdb343
Chosso <kurotak7255@gmail.com>
2024-06-22 17:38:19 +0900

    テーブル定義を変更したため、formクラスとentityクラスを修正

--- backend/backend/src/main/java/com/readrecords/backend/entity/ReadRecords.java
+++ backend/backend/src/main/java/com/readrecords/backend/entity/ReadRecords.java
@@ -10,17 +10,13 @@ import lombok.Data;
 @Entity
 @Table(name = "read_records")
 public class ReadRecords {
-  // 将来的にISBNを主キーとするが、一旦今は書籍名を主キーとする
-  // ISBNを主キーとする場合は、新しくEntityクラスに追加して差し替える形がいいか、今のを修正するのがいいかは要検討
   @Id
-  private Integer book_id;
-  private String book_name;
-  private String author;
+  private Integer record_id;
+  private String ISBN;
   private Integer user_id;
   private Date start_date;
   private Date end_date;
   private Integer read_count;
-  private String genre;
   private Integer priority;
   private String memo;
 }
--- backend/backend/src/main/java/com/readrecords/backend/form/BookRecordsForm.java
+++ backend/backend/src/main/java/com/readrecords/backend/form/BookRecordsForm.java
@@ -3,10 +3,14 @@ package com.readrecords.backend.form;
 import java.util.Date;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class BookRecordsForm {
+  @NotNull
+  @Size(max = 13)
+  private String ISBN;
   @NotNull
   private String book_name;
   @NotNull
