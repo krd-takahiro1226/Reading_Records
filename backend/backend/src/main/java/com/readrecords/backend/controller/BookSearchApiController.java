66a0fa2bb35fad27535280d2796fa8078f7c85e2
Chosso <kurotak7255@gmail.com>
2024-08-04 22:52:11 +0900

    楽天APIを使用したものに修正(パラメータの追加がメイン修正)

--- backend/backend/src/main/java/com/readrecords/backend/controller/BookSearchApiController.java
+++ backend/backend/src/main/java/com/readrecords/backend/controller/BookSearchApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.readrecords.backend.client.BookSearchApiClient;
+import com.readrecords.backend.entity.SearchBooksResponse;
 import com.readrecords.backend.entity.SearchRetrieveResponse;
 
 @Controller
@@ -16,8 +17,8 @@ import com.readrecords.backend.entity.SearchRetrieveResponse;
 public class BookSearchApiController {
   @Autowired BookSearchApiClient bookSearchApiClient;
   @GetMapping
-  public String showSearchWindow(@RequestParam String title, @RequestParam String creator, @RequestParam String isbn, Model model) throws Exception{
-    SearchRetrieveResponse response = bookSearchApiClient.getBookSearch(title, creator, isbn);
+  public String showSearchWindow(@RequestParam String title, @RequestParam String author, @RequestParam String publisherName,@RequestParam String isbn, Model model) throws Exception{
+    SearchBooksResponse response = bookSearchApiClient.getBookSearch(title, author, publisherName,isbn);
     model.addAttribute("response", response);
     return "searchResults";
 }
