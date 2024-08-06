e901aa03ddac7b7851f2b26e800026119f069e1b
Chosso <kurotak7255@gmail.com>
2024-08-06 20:01:29 +0900

    ユーザ作成用のformクラスを作成

---
+++ backend/backend/src/main/java/com/readrecords/backend/form/CreateUserForm.java
@@ -0,0 +1,20 @@
+package com.readrecords.backend.form;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+@Data
+public class CreateUserForm {
+  @NotNull
+  @Size(max = 50)
+  private String username;
+  @NotNull
+  @Email
+  private String email;
+  @NotNull
+  @Size(max = 255)
+  private String password;
+  @NotNull
+  @Size(max = 255)
+  private String confirmPassword;
+}
