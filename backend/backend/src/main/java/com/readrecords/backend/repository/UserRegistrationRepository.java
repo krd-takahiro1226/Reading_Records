ff8f7bb7b028f3b285750b27a6c99d5c479acb00
Chosso <kurotak7255@gmail.com>
2024-08-31 22:13:19 +0900

    ユーザIDが文字列に変わったことによる修正

--- backend/backend/src/main/java/com/readrecords/backend/repository/UserRegistrationRepository.java
+++ backend/backend/src/main/java/com/readrecords/backend/repository/UserRegistrationRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.readrecords.backend.entity.UserLogin;
 
 @Repository
-public interface  UserRegistrationRepository extends CrudRepository<UserLogin, Integer> {
+public interface  UserRegistrationRepository extends CrudRepository<UserLogin, String> {
   @Modifying
   @Query(value = "insert into users " + "(user_id, username, email, password) values " + "(:user_id, :username, :email, :hashPassword)", nativeQuery = true)
   void insertUserRecords(@Param("user_id")String user_id, @Param("username") String username, @Param("email") String email, @Param("hashPassword") String password);
