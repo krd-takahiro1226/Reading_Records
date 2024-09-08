1231626b0cc1eca03323c1cf9506ec5e0b50a742
Chosso <kurotak7255@gmail.com>
2024-09-08 22:31:54 +0900

    不要なログ出力部分を削除

--- backend/backend/src/main/java/com/readrecords/backend/controller/MenuController.java
+++ backend/backend/src/main/java/com/readrecords/backend/controller/MenuController.java
@@ -1,8 +1,6 @@
 package com.readrecords.backend.controller;
 
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -19,7 +17,6 @@ import com.readrecords.backend.service.ReadRecordsService;
 // 初期メニューから各種機能へ遷移させるためのController
 @Controller
 public class MenuController {
-  private static final Logger logger = LoggerFactory.getLogger(BackendController.class);
   @Autowired
   ReadRecordsService readRecordsService;
   @RequestMapping("/showRecords")
@@ -28,8 +25,6 @@ public class MenuController {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     UserLoginDetails userDetails = (UserLoginDetails) authentication.getPrincipal();
     String userId = userDetails.getUserId();
-    // System.out.println("ユーザーID: " + userId);
-    // ここでレコードが取れていないため、何も表示されない
     List<UserReadRecordsDto> readRecords = readRecordsService.getReadRecordsByUserId(userId);
     model.addAttribute("readRecords", readRecords);
     return "show";
